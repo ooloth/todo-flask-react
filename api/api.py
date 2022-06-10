9bba3f7df473d5333e821eff00aa38cc49833881
Michael Uloth <hello@michaeluloth.com>
2022-06-10 17:00:29 -0400

    add "update" logic

--- api/api.py
+++ api/api.py
@@ -4,8 +4,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 todos = [
-  {'id': 'abc', 'text': 'Buy milk'}, 
-  {'id': 'def', 'text': 'Learn Python'}
+  {'id': 'abc', 'text': 'Buy milk', 'editing': False}, 
+  {'id': 'def', 'text': 'Learn Python', 'editing': False}
 ]
 
 @app.route('/todos', methods=['GET'])
@@ -21,12 +21,14 @@ def create_todo():
 @app.route('/delete', methods=['POST'])
 def delete_todo():
   request_data = request.get_json()
-  print(request_data)
   todos.remove(request_data)
 
   return {'todos': todos}
 
 @app.route('/update', methods=['POST'])
 def update_todo():
-  todos[0] = 'Buy milk 2'
+  request_data = request.get_json()
+  print(request_data)
+  todo_index = todos.index(request_data['original_todo'])
+  todos[todo_index] = request_data['updated_todo']
   return {'todos': todos}
--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ function App() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ id: JSON.stringify(new Date()), text: todo }),
+      body: JSON.stringify({
+        id: JSON.stringify(new Date()),
+        text: todo,
+        editing: false,
+      }),
     })
       .then(res => res.json())
       .then(data => {
@@ -45,29 +49,93 @@ function App() {
       })
   }
 
-  function handleUpdate(e) {}
+  function handleEdit(e) {
+    const todoId = e.target.value
+
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === todoId) {
+        return { ...todo, editing: true }
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  function handleUpdate(e) {
+    e.preventDefault()
+    const updatedText = e.target.text.value
+    const originalTodo = todos.find(todo => todo)
+
+    fetch(`/update`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        original_todo: { ...originalTodo, editing: false },
+        updated_todo: { ...originalTodo, text: updatedText, editing: false },
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        setTodos(data.todos)
+      })
+  }
 
   return (
     <main>
       <h1>Todos</h1>
+
       <form onSubmit={handleSubmit}>
         <input type="text" name="text" />
         <button type="submit">Add</button>
       </form>
+
       <ul>
-        {todos.map(todo => {
-          console.log('todo', todo)
-          return (
-            <li key={todo.id}>
-              {todo.text}
-              <button value={JSON.stringify(todo)} onClick={handleDelete}>
-                X
-              </button>
-            </li>
-          )
-        })}
+        {todos.map(todo => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+            handleUpdate={handleUpdate}
+          />
+        ))}
       </ul>
     </main>
+  )
+}
+
+function Todo({ todo, handleDelete, handleEdit, handleUpdate }) {
+  const [text, setText] = useState(todo.text)
+
+  function handleTextChange(e) {
+    setText(e.target.value)
+  }
+  return (
+    <li>
+      {todo.editing ? (
+        <form onSubmit={handleUpdate}>
+          <input
+            type="text"
+            name="text"
+            value={text}
+            onChange={handleTextChange}
+          />
+          <button type="submit">Update</button>
+        </form>
+      ) : (
+        <p>{todo.text}</p>
+      )}
+
+      <button value={todo.id} onClick={handleEdit}>
+        Edit
+      </button>
+      <button value={JSON.stringify(todo)} onClick={handleDelete}>
+        X
+      </button>
+    </li>
   )
 }
 
