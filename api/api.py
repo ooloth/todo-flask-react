03425a9dfb16e6a757b6fb48782712b90dc5138e
Michael Uloth <hello@michaeluloth.com>
2022-06-10 15:40:01 -0400

    add "create" logic

--- api/api.py
+++ api/api.py
@@ -1,13 +1,26 @@
 import time
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-todos = ['Buy milk', 'Go to the gym', 'Learn Python']
+todos = [{'text': 'Buy milk'}, {'text':'Learn Python'}]
 
-@app.route('/create')
- 
-
-@app.route('/todos')
+@app.route('/todos', methods=['GET'])
 def get_todos():
-    return {'todos': todos}
+  return {'todos': todos}
+
+@app.route('/create', methods=['POST'])
+def create_todo():
+  request_data = request.get_json()
+  todos.append(request_data)
+  return {'todos': todos}
+
+@app.route('/delete', methods=['POST'])
+def delete_todo():
+  todos.pop()
+  return {'todos': todos}
+
+@app.route('/update', methods=['POST'])
+def update_todo():
+  todos[0] = 'Buy milk 2'
+  return {'todos': todos}
--- src/App.js
+++ src/App.js
@@ -7,16 +7,39 @@ function App() {
     fetch('/todos')
       .then(res => res.json())
       .then(data => {
+        console.log('data', data)
         setTodos(data.todos)
       })
   }, [])
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    const todo = e.target.text.value
+
+    fetch('/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: todo }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('data', data)
+        setTodos(data.todos)
+      })
+  }
+
   return (
     <main>
       <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="text" />
+        <button type="submit">Add</button>
+      </form>
       <ul>
         {todos.map(todo => (
-          <li>{todo}</li>
+          <li>{todo.text}</li>
         ))}
       </ul>
     </main>
